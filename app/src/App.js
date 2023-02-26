948c2c31d41df3f692d9aba6fcdecd2c7122f711
Marta Correia <48736172+martacorreia29@users.noreply.github.com>
2023-02-26 19:45:50 +0000

    menu

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './resources/logo.PNG';
 import './App.css';
 import TopButtons from "./components/TopButtons/TopButtons.js"
+import TextButton from "./components/TextButton.js"
 import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 
 function App() {
@@ -9,9 +10,13 @@ function App() {
       <div className="app" id="app">
         {true && 
           <TopButtons/>}        
-        <div className="startMenu">
+        <div className="content">
           <img src={logo} className="logo" alt="logo"/>
-
+          <div className="startMenu">
+            <TextButton text="Mapa"/>
+            <TextButton text="Catálogo"/>
+            <TextButton text="Sobre"/>
+          </div>
         </div > 
       </div>
     );
