589a725c37191442d68e824b0a4083343fcfbd30
Marta Correia <48736172+martacorreia29@users.noreply.github.com>
2023-03-01 21:57:44 +0000

    Updates

---
+++ precache-manifest.7c2d0fc6a414650e869d76f5079cc6d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0a50d8768bf5a8e1385",
+    "url": "/MonteDosCastelinhosWebAR/static/css/main.3b16022e.chunk.css"
+  },
+  {
+    "revision": "b0a50d8768bf5a8e1385",
+    "url": "/MonteDosCastelinhosWebAR/static/js/main.b0a50d87.chunk.js"
+  },
+  {
+    "revision": "adb2852fae2b928deb4c",
+    "url": "/MonteDosCastelinhosWebAR/static/js/1.adb2852f.chunk.js"
+  },
+  {
+    "revision": "5be22b23071e050c2979",
+    "url": "/MonteDosCastelinhosWebAR/static/js/2.5be22b23.chunk.js"
+  },
+  {
+    "revision": "43fa75b33f5477635336",
+    "url": "/MonteDosCastelinhosWebAR/static/js/runtime~main.43fa75b3.js"
+  },
+  {
+    "revision": "bd18e6bf4d9744314ef3d6d20d3819f7",
+    "url": "/MonteDosCastelinhosWebAR/static/media/logo.bd18e6bf.PNG"
+  },
+  {
+    "revision": "a75d8478660345a7852c0fa185992418",
+    "url": "/MonteDosCastelinhosWebAR/index.html"
+  }
+];
