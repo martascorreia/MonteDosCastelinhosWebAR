f97db841c1e7124c3033d4a90d4864803b6471ad
Marta Correia <marta.b.correia@accenture.com>
2023-04-28 15:57:54 +0100

    smaller model

--- app/src/pages/Sondagem4/Sondagem4.js
+++ app/src/pages/Sondagem4/Sondagem4.js
@@ -2,16 +2,17 @@ import React from 'react';
 import TopButtons from "../../components/TopButtons/TopButtons.js"
 import "../../index.css"
 import "./Sondagem4.css"
-import sondagem4Model from '../../resources/sondagem4 - noMontain.glb';
+import sondagem4Model from '../../resources/sondagem4 - smaller.glb';
+import {Scene, Entity} from '@belivvr/aframe-react';
 
 function Sondagem4() {
   return (
     <div className="Sondagem4">
       <TopButtons isHome={false} backUrl={"/MonteDosCastelinhosWebAR"} />
       <div className="content">
-        <a-scene>
-          <a-entity gltf-model={sondagem4Model} position="0 -500 -3000" rotation="0 255 0" scale="0.1 0.1 0.1"/>
-        </a-scene>
+        <Scene renderer="logarithmicDepthBuffer: true">
+          <a-entity gltf-model={sondagem4Model} position="1500 -700 1500" rotation="180 0 180" scale="0.5 0.5 0.5" />
+        </Scene>
       </div >
     </div>
   );
