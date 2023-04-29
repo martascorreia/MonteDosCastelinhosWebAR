6d72ece78b7d04a78c1e186eccb3234228540643
Marta Correia <marta.b.correia@accenture.com>
2023-04-30 00:21:05 +0100

    Updates

---
+++ precache-manifest.9361c1fb510da683d8e2e1363907a735.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "8967d79c252bc41aa0d1",
+    "url": "/MonteDosCastelinhosWebAR/static/css/main.36b6993e.chunk.css"
+  },
+  {
+    "revision": "8967d79c252bc41aa0d1",
+    "url": "/MonteDosCastelinhosWebAR/static/js/main.bc711827.chunk.js"
+  },
+  {
+    "revision": "9239028e67f36e329b75",
+    "url": "/MonteDosCastelinhosWebAR/static/js/runtime~main.32774311.js"
+  },
+  {
+    "revision": "a60d0df57b99f2ffce7d",
+    "url": "/MonteDosCastelinhosWebAR/static/css/2.6cce60bd.chunk.css"
+  },
+  {
+    "revision": "a60d0df57b99f2ffce7d",
+    "url": "/MonteDosCastelinhosWebAR/static/js/2.b28663aa.chunk.js"
+  },
+  {
+    "revision": "2b91d056f6b8971145c7",
+    "url": "/MonteDosCastelinhosWebAR/static/js/3.d9ff1bcd.chunk.js"
+  },
+  {
+    "revision": "fe7dd0090930c9f1a861c623eefa409d",
+    "url": "/MonteDosCastelinhosWebAR/static/media/sondagem4 - smaller.fe7dd009.glb"
+  },
+  {
+    "revision": "17ec77e988516c9374199f038f28746f",
+    "url": "/MonteDosCastelinhosWebAR/static/media/logo2.17ec77e9.PNG"
+  },
+  {
+    "revision": "5e2689c56ae325c98d240cad316a28ff",
+    "url": "/MonteDosCastelinhosWebAR/static/media/lexend-deca-vietnamese-400-normal.5e2689c5.woff2"
+  },
+  {
+    "revision": "b717ce3a586af7dfb798d53d8dc43f96",
+    "url": "/MonteDosCastelinhosWebAR/static/media/lexend-deca-latin-ext-400-normal.b717ce3a.woff2"
+  },
+  {
+    "revision": "9244c3d483aec06e564cbc9d79e03afa",
+    "url": "/MonteDosCastelinhosWebAR/static/media/lexend-deca-all-400-normal.9244c3d4.woff"
+  },
+  {
+    "revision": "ad657b82fc91d41834fc30b1774fcbf9",
+    "url": "/MonteDosCastelinhosWebAR/static/media/lexend-deca-latin-400-normal.ad657b82.woff2"
+  },
+  {
+    "revision": "e8d2bb29e4bd31e5d8d0fbf5dc48d216",
+    "url": "/MonteDosCastelinhosWebAR/index.html"
+  }
+];
