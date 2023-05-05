0fe034200a65f2b4fceef45cb601a68159784a02
Marta Correia <marta.b.correia@accenture.com>
2023-05-05 17:22:07 +0100

    more

--- app/src/pages/RealidadeAumentada/RealidadeAumentada.js
+++ app/src/pages/RealidadeAumentada/RealidadeAumentada.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TopButtons from "../../components/TopButtons/TopButtons.js"
 import "../../index.css"
 import "./RealidadeAumentada.css"
-import sondagem4Model from '../../resources/sondagem4 - smaller.glb';
+import sondagem4Model from '../../resources/sondagem4 - smaller w sub roof.glb';
 
 function RealidadeAumentada() {
   return (
@@ -10,8 +10,7 @@ function RealidadeAumentada() {
       <TopButtons backUrl={"/MonteDosCastelinhosWebAR"} />
       <div className="content">
         <a-scene renderer="logarithmicDepthBuffer: true" vr-mode-ui='enabled: false' embedded arjs='sourceType: webcam;'>
-          <a-entity gltf-model={sondagem4Model} position="0 0 -10" rotation="0 180 -6" scale="1.5 0.5 1.5"/>
-          <a-camera gps-new-camera='gpsMaxDistance: 5' rotation-reader />
+          <a-entity gltf-model={sondagem4Model} position="0 1.5 -10" rotation="0 180 -6" scale="0.5 0.5 1.5"/>
         </a-scene>
       </div >
     </div>
