e4e3ef7582a3efa7bc2af5d53ef258a83bca0fba
Marta Correia <marta.b.correia@accenture.com>
2023-05-21 16:13:44 +0100

    correct module

--- app/src/pages/RealidadeAumentada/RealidadeAumentada.js
+++ app/src/pages/RealidadeAumentada/RealidadeAumentada.js
@@ -2,15 +2,23 @@ import React from 'react';
 import TopButtons from "../../components/TopButtons/TopButtons.js"
 import "../../index.css"
 import "./RealidadeAumentada.css"
-import sondagem4Model from '../../resources/sondagem4 - smaller w sub roof.glb';
+import sondagem4Model from '../../resources/sondagem4 - smaller.glb';
 
 function RealidadeAumentada() {
+  screen.orientation.lock("landscape")
+	.then(function() {
+		alert('Locked');
+	})
+	.catch(function(error) {
+	});
+
   return (
     <div className="RealidadeAumentada">
       <TopButtons backUrl={"/MonteDosCastelinhosWebAR"} />
       <div className="content">
         <a-scene renderer="logarithmicDepthBuffer: true" vr-mode-ui='enabled: false' embedded arjs='sourceType: webcam;'>
-          <a-entity gltf-model={sondagem4Model} position="0 1.5 -10" rotation="0 180 -6" scale="0.5 0.5 1.5"/>
+          <a-entity gltf-model={sondagem4Model} position="0 -80 0" rotation="0 180 -6" scale="1 1 1" gps-new-entity-place="latitude: 38.999076; longitude: -9.007012"/>
+          <a-camera arjs-device-orientation-controls='smoothingFactor: 0.1'> </a-camera>
         </a-scene>
       </div >
     </div>
--- app/src/pages/RealidadeVirtual/RealidadeVirtual.js
+++ app/src/pages/RealidadeVirtual/RealidadeVirtual.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TopButtons from "../../components/TopButtons/TopButtons.js"
 import "../../index.css"
 import "./RealidadeVirtual.css"
-import sondagem4Model from '../../resources/sondagem4 - smaller w sub roof.glb';
+import sondagem4Model from '../../resources/sondagem4 - smaller.glb';
 
 function RealidadeVirtual() {
   return (
@@ -10,8 +10,8 @@ function RealidadeVirtual() {
       <TopButtons backUrl={"/MonteDosCastelinhosWebAR"} />
       <div className="content">
         <a-scene renderer="logarithmicDepthBuffer: true">
-          <a-entity gltf-model={sondagem4Model} position="0 1.5 -10" rotation="0 180 -6" scale="0.5 0.5 0.5"/>
-          <a-entity camera look-controls position="0 1.6 0"></a-entity>
+          <a-entity gltf-model={sondagem4Model} position="0 0 -10" rotation="0 180 -6" scale="0.5 0.5 0.5"/>
+          <a-entity camera look-controls position="0 1.6 0"/>
         </a-scene>
       </div >
     </div>
