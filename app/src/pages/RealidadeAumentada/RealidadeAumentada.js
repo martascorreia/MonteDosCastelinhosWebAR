e2af2f8a097bdeb3b747ef145abcf6b4c29fc32a
Marta Correia <marta.b.correia@accenture.com>
2023-05-22 18:47:01 +0100

    AR - image assisted alignment

--- app/src/pages/RealidadeAumentada/RealidadeAumentada.js
+++ app/src/pages/RealidadeAumentada/RealidadeAumentada.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useState } from "react";
+
 import TopButtons from "../../components/TopButtons/TopButtons.js"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../../index.css"
 import "./RealidadeAumentada.css"
 import sondagem4Model from '../../resources/sondagem4 - smaller.glb';
+import sondagem4Img from '../../resources/sondagem4.png';
+
 
 function RealidadeAumentada() {
   screen.orientation.lock("landscape")
@@ -12,13 +17,25 @@ function RealidadeAumentada() {
 	.catch(function(error) {
 	});
 
+  const [modelAligned, setModelAligned] = useState(false);
+
+
   return (
     <div className="RealidadeAumentada">
       <TopButtons backUrl={"/MonteDosCastelinhosWebAR"} />
       <div className="content">
-        <a-scene renderer="logarithmicDepthBuffer: true" vr-mode-ui='enabled: false' embedded arjs='sourceType: webcam;'>
-          <a-entity gltf-model={sondagem4Model} position="0 -80 0" rotation="0 180 -6" scale="1 1 1" gps-new-entity-place="latitude: 38.999076; longitude: -9.007012"/>
-          <a-camera arjs-device-orientation-controls='smoothingFactor: 0.1'> </a-camera>
+        <a-scene renderer="logarithmicDepthBuffer: true; antialias: true; alpha: true" vr-mode-ui='enabled: false' embedded arjs='sourceType: webcam;'>
+          <a-camera arjs-device-orientation-controls='smoothingFactor: 0.1'/>
+            {!modelAligned && 
+              <div className="alignElements">
+                <img className="alignImage" src={sondagem4Img}/>
+                <button className="alignedBtn" onClick={() => setModelAligned(true)}>
+                  Alinhado
+                  <FontAwesomeIcon icon="fa-solid fa-house"/>
+                </button>  
+              </div>}
+            {modelAligned && 
+              <a-entity gltf-model={sondagem4Model} position="0 -80 0" rotation="0 180 -6" scale="1 1 1"/>}
         </a-scene>
       </div >
     </div>
