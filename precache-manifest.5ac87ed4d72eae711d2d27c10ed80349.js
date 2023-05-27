873260b690b2e9d43921b6ed6636add7620c6e41
Marta Correia <48736172+martacorreia29@users.noreply.github.com>
2023-05-27 15:28:31 +0100

    Updates

---
+++ precache-manifest.5ac87ed4d72eae711d2d27c10ed80349.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "7544093d0e5947ddc4a7",
+    "url": "/MonteDosCastelinhosWebAR/static/css/main.761bd757.chunk.css"
+  },
+  {
+    "revision": "7544093d0e5947ddc4a7",
+    "url": "/MonteDosCastelinhosWebAR/static/js/main.a12878ca.chunk.js"
+  },
+  {
+    "revision": "8a417db56e36b76b0fea",
+    "url": "/MonteDosCastelinhosWebAR/static/js/runtime~main.96961a79.js"
+  },
+  {
+    "revision": "a4b0e873495732572866",
+    "url": "/MonteDosCastelinhosWebAR/static/css/2.33667305.chunk.css"
+  },
+  {
+    "revision": "a4b0e873495732572866",
+    "url": "/MonteDosCastelinhosWebAR/static/js/2.48adad33.chunk.js"
+  },
+  {
+    "revision": "f4866491ae15624a43d3",
+    "url": "/MonteDosCastelinhosWebAR/static/js/3.b700955a.chunk.js"
+  },
+  {
+    "revision": "917f1ef6637687bb01ac94a797f95684",
+    "url": "/MonteDosCastelinhosWebAR/static/media/sondagem4 - smaller.917f1ef6.glb"
+  },
+  {
+    "revision": "17ec77e988516c9374199f038f28746f",
+    "url": "/MonteDosCastelinhosWebAR/static/media/logo2.17ec77e9.PNG"
+  },
+  {
+    "revision": "9244c3d483aec06e564cbc9d79e03afa",
+    "url": "/MonteDosCastelinhosWebAR/static/media/lexend-deca-all-400-normal.9244c3d4.woff"
+  },
+  {
+    "revision": "5e2689c56ae325c98d240cad316a28ff",
+    "url": "/MonteDosCastelinhosWebAR/static/media/lexend-deca-vietnamese-400-normal.5e2689c5.woff2"
+  },
+  {
+    "revision": "b717ce3a586af7dfb798d53d8dc43f96",
+    "url": "/MonteDosCastelinhosWebAR/static/media/lexend-deca-latin-ext-400-normal.b717ce3a.woff2"
+  },
+  {
+    "revision": "ad657b82fc91d41834fc30b1774fcbf9",
+    "url": "/MonteDosCastelinhosWebAR/static/media/lexend-deca-latin-400-normal.ad657b82.woff2"
+  },
+  {
+    "revision": "b95b37127591d460eb198cbd6921246d",
+    "url": "/MonteDosCastelinhosWebAR/static/media/sondagem4.b95b3712.png"
+  },
+  {
+    "revision": "63fb78f536f59dae6d9ec51782921010",
+    "url": "/MonteDosCastelinhosWebAR/index.html"
+  }
+];
