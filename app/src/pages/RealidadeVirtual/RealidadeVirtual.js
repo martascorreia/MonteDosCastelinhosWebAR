6259d1dda38f58ee3b4151946273cdc8e4d32155
Marta Correia <marta.b.correia@accenture.com>
2023-07-22 23:26:20 +0100

    vr starting point fixed

--- app/src/pages/RealidadeVirtual/RealidadeVirtual.js
+++ app/src/pages/RealidadeVirtual/RealidadeVirtual.js
@@ -10,8 +10,8 @@ function RealidadeVirtual() {
       <TopButtons backUrl={"/MonteDosCastelinhosWebAR"} />
       <div className="content">
         <a-scene renderer="logarithmicDepthBuffer: true">
-          <a-entity gltf-model={sondagem4Model} position="0 0 -10" rotation="0 180 -6" scale="0.5 0.5 0.5"/>
-          <a-entity camera look-controls position="0 1.6 0"/>
+          <a-entity gltf-model={sondagem4Model} position="5 -15 -45" rotation="0 250 0" scale="0.5 0.5 0.5"/>
+          <a-entity camera look-controls/>
         </a-scene>
       </div >
     </div>
