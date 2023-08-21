ba02b1b3579bd9eda0e703d0d8a4dca28d2354a6
Marta Correia <marta.b.correia@accenture.com>
2023-08-21 23:06:19 +0100

    renaming

--- app/src/pages/AugmentedReality/AugmentedReality41.js
+++ app/src/pages/AugmentedReality/AugmentedReality41.js
@@ -1,12 +1,12 @@
 import React, { useState, useRef, useEffect, useCallback } from 'react';
 import TopButtons from "../../components/TopButtons/TopButtons.js"
 import "../../index.css"
-import "./RealidadeAumentada41.css"
+import "./AugmentedReality.css"
 import sondagem4Img from '../../resources/images/alignmentImages/sondagem4.1.png';
 import sondagem4Model from '../../resources/models/sondagem4.smaller.glb';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-function RealidadeAumentada() {
+function AugmentedReality41() {
   const [modelAligned, setModelAligned] = useState(false);
   const [modelLoaded, setModelLoaded] = useState(false);
   const [model, setModel] = useState(null);
@@ -82,20 +82,21 @@ function RealidadeAumentada() {
 
   return (
     <div className="RealidadeAumentada">
-      <TopButtons cleanUp={cleanupModel} backUrl={"/MonteDosCastelinhosWebAR/sondagem4"} />
+      <TopButtons cleanUp={handleCleanup} backUrl={"/MonteDosCastelinhosWebAR/sondagem4"} />
       <div className="content">
-        {modelAligned &&
-          <a-scene embedded renderer="antialias: true; logarithmicDepthBuffer: true; colorManagement: false; sortObjects: true;" vr-mode-ui='enabled: false'>
-            <a-camera rotation-reader look-controls="touchEnabled: false; mouseEnabled: false;" />
+        <a-scene embedded renderer="antialias: true; logarithmicDepthBuffer: true; colorManagement: false; sortObjects: true;" vr-mode-ui='enabled: false'>
+          <a-camera rotation-reader look-controls="touchEnabled: false; mouseEnabled: false;" />
+          {modelAligned &&
             <a-entity
+              className="model"
               ref={entityRef}
               geometry-merger
-              material="shader: flat" />
+              material="shader: flat" />}
+          {modelAligned &&
             <div className="alignElements">
-              <img className="backgroundImage" src={sondagem4Img} />
-            </div>
-          </a-scene>
-        }
+              <img className="alignImage" src={sondagem4Img} />
+            </div>}
+        </a-scene>
         {!modelAligned &&
           <div className="alignElements">
             <img className="alignImage" src={sondagem4Img} />
@@ -107,4 +108,4 @@ function RealidadeAumentada() {
     </div>
   );
 }
-export default RealidadeAumentada;
+export default AugmentedReality41;
