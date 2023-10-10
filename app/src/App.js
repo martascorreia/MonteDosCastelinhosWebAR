45e3c9ca966a85034d6d06eca54604a193986ec3
Marta Correia <marta.b.correia@accenture.com>
2023-10-10 15:28:51 +0100

    page 404

--- app/src/App.js
+++ app/src/App.js
@@ -14,20 +14,20 @@ function App() {
     <div className="App" id="app">
       <BrowserRouter >
         <Routes>
-          <Route path='/*' element={<Home />} />
+          <Route path="*" element={<p>Page not found</p>} />
           <Route path='/MonteDosCastelinhosWebAR' element={<Home />} />
-          <Route path='/MonteDosCastelinhosWebAR/mapa' element={<Map />}/>
-          <Route path='/MonteDosCastelinhosWebAR/sondagem4' element={getSondagem("4", 1)}/>
-          <Route path='/MonteDosCastelinhosWebAR/sondagem4/raTab' element={getSondagem("4", 2)}/>
-          <Route path='/MonteDosCastelinhosWebAR/sondagem4/rvTab' element={getSondagem("4", 3)}/>
+          <Route path='/MonteDosCastelinhosWebAR/mapa' element={<Map />} />
+          <Route path='/MonteDosCastelinhosWebAR/sondagem4' element={getSondagem("4", 1)} />
+          <Route path='/MonteDosCastelinhosWebAR/sondagem4/raTab' element={getSondagem("4", 2)} />
+          <Route path='/MonteDosCastelinhosWebAR/sondagem4/rvTab' element={getSondagem("4", 3)} />
           <Route path='/MonteDosCastelinhosWebAR/sondagem4/raA' element={<Sondagem4A />} />
           <Route path='/MonteDosCastelinhosWebAR/sondagem4/raB' element={<Sondagem4B />} />
           <Route path='/MonteDosCastelinhosWebAR/sondagem4/raC' element={<Sondagem4C />} />
-          <Route path='/MonteDosCastelinhosWebAR/sondagem4/rvA' element={<Sondagem4VR id={1}/>} />
-          <Route path='/MonteDosCastelinhosWebAR/sondagem4/rvB' element={<Sondagem4VR id={2}/>} />
-          <Route path='/MonteDosCastelinhosWebAR/sondagem4/rvC' element={<Sondagem4VR id={3}/>} />
-          <Route path='/MonteDosCastelinhosWebAR/sondagem4/rvD' element={<Sondagem4VR id={4}/>} />
-          <Route path='/MonteDosCastelinhosWebAR/sondagem4/rvE' element={<Sondagem4VR id={5}/>} />
+          <Route path='/MonteDosCastelinhosWebAR/sondagem4/rvA' element={<Sondagem4VR id={1} />} />
+          <Route path='/MonteDosCastelinhosWebAR/sondagem4/rvB' element={<Sondagem4VR id={2} />} />
+          <Route path='/MonteDosCastelinhosWebAR/sondagem4/rvC' element={<Sondagem4VR id={3} />} />
+          <Route path='/MonteDosCastelinhosWebAR/sondagem4/rvD' element={<Sondagem4VR id={4} />} />
+          <Route path='/MonteDosCastelinhosWebAR/sondagem4/rvE' element={<Sondagem4VR id={5} />} />
           <Route path='/MonteDosCastelinhosWebAR/sondagem5' element={getSondagem("5", 1)} />
           <Route path='/MonteDosCastelinhosWebAR/sondagem8' element={getSondagem("8", 1)} />
           <Route path='/MonteDosCastelinhosWebAR/sondagem9' element={getSondagem("9", 1)} />
