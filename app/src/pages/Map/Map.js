da206ad1635519c744e8c0827e37b4339f63ae59
Marta Correia <marta.b.correia@accenture.com>
2023-12-08 15:36:15 +0000

    [NEW] reversed the main map

--- app/src/pages/Map/Map.js
+++ app/src/pages/Map/Map.js
@@ -18,24 +18,24 @@ function Map() {
           <div className='sondagemTitle'>Mapa</div>
         </div>
         <div className='mainMapMap'>
-          <img className='mainMapImage' src={process.env.PUBLIC_URL + "/images/Maps/mainMap.png"} />
+          <img className='mainMapImage' src={process.env.PUBLIC_URL + "/images/Maps/mainMapReversed.png"} />
           <Link to={'/sondagem4/'} className="nav-link">
-            <button className='mapPoint mainMapPoint4'>
+            <button className='mapPoint mainMapPoint4Reversed'>
               <img className='mapPoints' src={process.env.PUBLIC_URL + "/images/MapPoints/point4.png"} />
             </button>
           </Link>
           <Link to={'/sondagem5/'} className="nav-link">
-            <button className='mapPoint mainMapPoint5'>
+            <button className='mapPoint mainMapPoint5Reversed'>
               <img className='mapPoints' src={process.env.PUBLIC_URL + "/images/MapPoints/point2.png"} />
             </button>
           </Link>
           <Link to={'/sondagem8/'} className="nav-link">
-            <button className='mapPoint mainMapPoint8'>
+            <button className='mapPoint mainMapPoint8Reversed'>
               <img className='mapPoints' src={process.env.PUBLIC_URL + "/images/MapPoints/point3.png"} />
             </button>
           </Link>
           <Link to={'/sondagem9/'} className="nav-link">
-            <button className='mapPoint mainMapPoint9'>
+            <button className='mapPoint mainMapPoint9Reversed'>
               <img className='mapPoints' src={process.env.PUBLIC_URL + "/images/MapPoints/point1.png"} />
             </button>
           </Link>
