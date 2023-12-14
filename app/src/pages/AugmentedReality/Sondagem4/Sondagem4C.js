6019cae5f22bbe59604dd74084242bb716047d84
Marta Correia <marta.b.correia@accenture.com>
2023-12-14 23:15:50 +0000

    [WIP] back up

--- app/src/pages/AugmentedReality/Sondagem4/Sondagem4C.js
+++ app/src/pages/AugmentedReality/Sondagem4/Sondagem4C.js
@@ -3,9 +3,13 @@ import TopButtons from "../../../components/TopButtons/TopButtons.js"
 import LoadingScreen from "../../../components/LoadingScreen/LoadingScreen.js"
 import "../../../index.css"
 import "./../AugmentedReality.css"
+import sondagem4Model from '../../../resources/models/sondagem4.smaller.glb';
+import sondagem4ModelStraight from '../../../resources/models/sondagem4.smaller.straight.glb';
 import sondagem4Img from '../../../resources/images/alignmentImages/sondagem4C.png';
-import { setOrientation, loadModel, cleanCamera, cleanModel } from '../../../utils/utils.js';
+import { setOrientation, loadModel, cleanCamera, handleCleanup, setFullScreen } from '../../../utils/utils.js';
 import AligmentButton from '../../../components/AlignmentButton/AligmnentButton.js';
+import * as THREE from 'three';
+import { Math as ThreeMath } from 'three';
 
 function Sondagem4C({ backUrl }) {
   setOrientation("landscape");
@@ -14,9 +18,15 @@ function Sondagem4C({ backUrl }) {
   const [isModelSet, setIsModelSet] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const entityRef = useRef();
+  const alignmentCameraRef = useRef();
+  const [alignmentCameraPosition, setAlignmentCameraPosition] = useState(null);
+  const [alignmentCameraRotation, setAlignmentCameraRotation] = useState(null);
+  const cameraRef = useRef();
   const [label, setLabel] = useState(null);
 
-  const handleButtonClick = () => {
+  const handleModelAligned = () => {
+    setAlignmentCameraPosition(alignmentCameraRef.current.object3D.position.clone());
+    setAlignmentCameraRotation(alignmentCameraRef.current.object3D.rotation.clone());
     cleanCamera();
     setModelAligned(true);
     setLabel("Vista da Retaguarda");
@@ -36,7 +46,7 @@ function Sondagem4C({ backUrl }) {
 
   // Load model
   const load3DModel = () => {
-    loadModel(process.env.PUBLIC_URL + '/models/sondagem4.smaller.glb')
+    loadModel(sondagem4ModelStraight)
       .then((loadedModel) => {
         setModel(loadedModel);
         setIsLoading(false);
@@ -48,14 +58,61 @@ function Sondagem4C({ backUrl }) {
 
   // Set model
   const setModelInScene = () => {
-    if (entityRef.current) {
+    if (entityRef.current && cameraRef.current) {
       entityRef.current.object3D.add(model);
+
+      //position
+      //alignmentCameraPosition.y × 180°/π = 57.29578°
+      const cameraPosition = alignmentCameraPosition;
       entityRef.current.object3D.position.set(85, -145, -660);
       entityRef.current.object3D.scale.set(3, 3.33, 3);
-      entityRef.current.setAttribute('rotation', '5 65 17');
+      entityRef.current.setAttribute('rotation', '-5 90 10');
+      cameraRef.current.setAttribute('rotation', '0 0 0');
+
+      let controls = document.querySelector('a-camera').components['look-controls']
+      controls.pitchObject.rotation.x = 0
+      controls.yawObject.rotation.y = 0
+
       setIsModelSet(true)
     }
   };
+
+  // Set model
+  /*const setModelInScene = () => {
+    if (entityRef.current && cameraRef.current) {
+      // Get the camera's position and rotation
+      const cameraPosition = alignmentCameraPosition;
+      const cameraRotation = alignmentCameraRotation;
+
+      console.log(alignmentCameraPosition, alignmentCameraRotation);
+
+      // Convert rotation to radians
+      const rotationRad = {
+        x: cameraRotation.x,
+        y: cameraRotation.y,
+        z: cameraRotation.z,
+      };
+      
+      // Calculate the position in front of the camera
+      const distance = 0 // Adjust the distance as needed
+      const frontPosition = {
+        x: cameraPosition.x + distance * Math.sin(rotationRad.y),
+        y: cameraPosition.y + distance * Math.sin(rotationRad.x),
+        z: cameraPosition.z - distance * Math.cos(rotationRad.y)
+      };
+
+      console.log(frontPosition);
+
+      // Set the object's position
+      entityRef.current.object3D.position.set(85, -145, -660);
+
+      // Set other properties as needed
+      entityRef.current.object3D.scale.set(3, 3.33, 3);
+      entityRef.current.setAttribute('rotation', '5 65 17');
+
+      setIsModelSet(true);
+    }
+  };*/
 
   const cleanUp = () => {
     handleCleanup(model, entityRef, document.querySelectorAll('a-scene'));
@@ -63,8 +120,24 @@ function Sondagem4C({ backUrl }) {
     setFullScreen(false, true);
   };
 
+  const printEntityPosition = () => {
+    if (entityRef.current) {
+      /*console.log('Model Position:', entityRef.current.object3D.position);
+      console.log('Model Rotation:', entityRef.current.object3D.rotation);*/
+    }
+    if (cameraRef.current) {
+      console.log('Camera Position:', cameraRef.current.object3D.position);
+      console.log('Camera Rotation:', cameraRef.current.object3D.rotation);
     }
   };
+
+  useEffect(() => {
+    const intervalId = setInterval(printEntityPosition, 10000);
+    return () => {
+      // Clean up interval on component unmount
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return (
     <div className="AugmentedReality">
@@ -76,18 +149,25 @@ function Sondagem4C({ backUrl }) {
           <a-scene className="scene"
             embedded
             renderer="antialias: true; logarithmicDepthBuffer: true; colorManagement: false; sortObjects: true;"
-            vr-mode-ui='enabled: false'>
-            <a-camera
-              position="0 0 0"
-              rotation="0 0 0"
-              look-controls="touchEnabled: true; mouseEnabled: true;">
-              <a-entity rotation="0 0 0" />
-            </a-camera>
-            <a-entity
-              className="model"
-              ref={entityRef}
-              geometry-merger
-            />
+            xr-mode-ui='enabled: false'>
+            <a-entity>
+              {/*<a-camera
+                position="0 0 0"
+                rotation="0 0 0"
+                ref={cameraRef}
+                look-controls="touchEnabled: true; mouseEnabled: true;">
+      </a-camera>*/}
+              <a-entity position="0 0 0" rotation="0 0 0">
+                <a-camera
+                  ref={cameraRef} look-controls='enabled: true' />
+              </a-entity>
+              <a-entity
+                className="model"
+                ref={entityRef}
+                geometry-merger
+                anchored="persistent: true"
+              />
+            </a-entity>
             {true &&
               <div className="alignElements">
                 <img className="alignImage" src={sondagem4Img} />
@@ -99,12 +179,12 @@ function Sondagem4C({ backUrl }) {
           <a-scene
             className="scene"
             renderer="antialias: true; logarithmicDepthBuffer: true; colorManagement: false; sortObjects: true;"
-            vr-mode-ui='enabled: false'>
-            <a-camera />
+            xr-mode-ui='enabled: false'>
+            <a-camera ref={alignmentCameraRef} />
           </a-scene>
           <div className="alignElements">
             <img className="alignImage" src={sondagem4Img} />
-            <AligmentButton onClick={handleButtonClick} />
+            <AligmentButton onClick={() => handleModelAligned()} />
           </div>
         </div>}
     </div>
