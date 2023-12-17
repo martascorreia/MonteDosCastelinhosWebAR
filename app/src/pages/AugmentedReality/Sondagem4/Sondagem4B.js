9a6a7a6d0f2156d9e6bcaa6f052dc5c8dc2f9099
Marta Correia <marta.b.correia@accenture.com>
2023-12-17 19:47:56 +0000

    [NEW] adding clues for alginment + sondagemB model aligned with correct angle

--- app/src/pages/AugmentedReality/Sondagem4/Sondagem4B.js
+++ app/src/pages/AugmentedReality/Sondagem4/Sondagem4B.js
@@ -1,21 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import TopButtons from "../../../components/TopButtons/TopButtons.js"
 import "../../../index.css"
 import "./../AugmentedReality.css"
-import sondagem4Img from '../../../resources/images/alignmentImages/sondagem4B.png';
+import sondagem4Img from '../../../resources/images/alignmentImages/sondagemBCorrect.jpeg';
 import sondagem4B from '../../../resources/models/sondagem4B.png';
 import { setOrientation, cleanCamera } from '../../../utils/utils.js';
 import AligmentButton from '../../../components/AlignmentButton/AligmnentButton.js';
 
-function Sondagem4B({backUrl}) {
+function Sondagem4B({ backUrl }) {
   setOrientation("landscape");
   const [modelAligned, setModelAligned] = useState(false);
   const [label, setLabel] = useState(null);
+  const entityRef = useRef();
+  const entityParentRef = useRef();
+  const cameraRef = useRef();
+  const [cameraOrientation, setCameraOrientation] = useState(null);
 
-  const handleButtonClick = () => {
-    cleanCamera();
+  const handleModelAligned = () => {
+    if (cameraRef.current)
+      setCameraOrientation(cameraRef.current.object3D.rotation.clone());
     setModelAligned(true);
     setLabel("Vista Central");
+  };
+
+  useEffect(() => {
+    localStorage.setItem('sondagem4ARBFlag', 'true');
+    localStorage.setItem('hasRefreshed', 'false');
+
+    if (modelAligned) {
+      setModelInScene();
+    }
+  }, [modelAligned]);
+
+  // Set model
+  const setModelInScene = () => {
+    if (cameraRef.current && entityParentRef.current) {
+      entityParentRef.current.object3D.rotation.set(cameraOrientation.x, cameraOrientation.y, cameraOrientation.z);
+    }
   };
 
   // Cleanup resources
@@ -28,54 +49,51 @@ function Sondagem4B({backUrl}) {
 
     // clean camera
     cleanCamera();
-  };
 
-  useEffect(() => {
-    localStorage.setItem('sondagem4ARBFlag', 'true');
-    localStorage.setItem('hasRefreshed', 'false');
-  });
+    //references
+    entityRef = null;
+    cameraRef = null;
+    entityParentRef = null;
+  };
 
   return (
     <div className="AugmentedReality">
       <TopButtons hideFullScreenButton={true} cleanUp={handleCleanup} backUrl={backUrl} label={label} />
-      {modelAligned &&
-        <div className="content">
-          <a-scene className="scene"
-            embedded
-            renderer="antialias: true; logarithmicDepthBuffer: true; colorManagement: false; sortTransparentObjects: true;"
-            xr-mode-ui='enabled: false'>
+      <div className="content">
+        <a-scene className="scene"
+          embedded
+          renderer="antialias: true; logarithmicDepthBuffer: true; colorManagement: false; sortTransparentObjects: true;"
+          xr-mode-ui='enabled: false'>
+          <a-entity position="0 0 0" rotation="0 0 0">
             <a-camera
-              position="0 0 0"
-              rotation="-30 -100 0"
-              look-controls="touchEnabled: true; mouseEnabled: true;" />
-            <a-entity rotation="0 0 0">
+              ref={cameraRef} look-controls='enabled: true;' />
+          </a-entity>
+          {modelAligned &&
+            <a-entity ref={entityParentRef}>
               <a-image
                 src={sondagem4B}
-                position="0 0 -7.5"
+                ref={entityRef}
                 scale="30 7 0"
-                rotation="0 0 -3"
+                rotation="0 0 0"
+                position="0 -3 -7.5"
                 material="transparent: true; blending: normal"
               />
-              {true &&
-                <div className="alignElements">
-                  <img className="alignImage" src={sondagem4Img} />
-                </div>}
-            </a-entity>
-          </a-scene>
-        </div>}
-      {!modelAligned &&
-        <div className="content">
-          <a-scene
-            className="scene"
-            renderer="antialias: true; logarithmicDepthBuffer: true; colorManagement: false; sortTransparentObjects: true;"
-            xr-mode-ui='enabled: false'>
-            <a-camera />
-          </a-scene>
-          <div className="alignElements">
-            <img className="alignImage" src={sondagem4Img} />
-            <AligmentButton onClick={handleButtonClick}/>
-          </div>
-        </div>}
+            </a-entity>}
+          {!modelAligned &&
+            <div className="alignElements">
+              <img className="alignImage" src={sondagem4Img} />
+              <AligmentButton onClick={() => handleModelAligned()} />
+              <a className='aligmentText'>
+                <b>Dica:</b> Alinhe-se entre as duas árvores acima, no centro do espaço vazio.
+              </a>
+            </div>}
+          {modelAligned && false &&
+            <div className="alignElements">
+              <img className="alignImage" src={sondagem4Img} />
+
+            </div>}
+        </a-scene>
+      </div>
     </div >
   );
 }
