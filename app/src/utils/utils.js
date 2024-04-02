f37161d0c88611d4e77c8f187d5bca8e9e4585ec
Marta Correia <marta.b.correia@accenture.com>
2024-04-02 17:43:06 +0100

    general oprientation

--- app/src/utils/utils.js
+++ app/src/utils/utils.js
@@ -1,12 +1,17 @@
 export const setOrientation = (type) => {
     //const isMobile = navigator.userAgentData.mobile;
     // if (!isMobile) return;
-    if (screen.orientation !== undefined) {
+   /* if (screen.orientation !== undefined && screen.orientation.lock !== undefined) {
         var typeLock = type == "landscape" ? "landscape-primary" : "portrait-primary"
         screen.orientation.lock(typeLock)
             .then(function () { })
             .catch(function (error) { });
-    }
+    }*/
+
+    var typeLock = type == "landscape" ? "landscape-primary" : "portrait-primary"
+    o9n.orientation.lock(typeLock)
+            .then(function () { })
+            .catch(function (error) { });
 }
 
 export const reloadPage = () => {
